67627bac220e50ac17e03af0a564cd816cf80168
Santiago <santiagojavierveiga@gmail.com>
2021-09-10 16:46:24 -0300

    feat(client/Redux): added constant style to button

---
+++ constant/buttonStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    margin: 10
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#7bab65",
+    padding: 10,
+    margin: 10
+  },
+});
