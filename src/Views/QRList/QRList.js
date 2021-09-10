83107664a384d63862349681832160031c8bc96c
Santiago <santiagojavierveiga@gmail.com>
2021-09-10 11:05:56 -0300

    feat(client/QRList): added QRList view

---
+++ src/Views/QRList/QRList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+export function QRList() {
+  return (
+    <View>
+      <Text>This is the 'QRList' view</Text>
+    </View>
+  )
+}
