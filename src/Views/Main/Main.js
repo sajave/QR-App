8089b072539a1d6fa6d2e10d0ab9989f8bc7cbd7
Santiago <santiagojavierveiga@gmail.com>
2021-09-10 13:39:41 -0300

    feat(client/Main): added icons to tab bar

--- src/Views/Main/Main.js
+++ src/Views/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ReadQR } from '../ReadQR/ReadQR';
 import { QRList } from '../QRList/QRList';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +13,21 @@ export function Main() {
       tabBarOptions={{
         activeTintColor: 'darkred'
       }}>
-      <Tab.Screen name='ReadQR' component={ReadQR} />
-      <Tab.Screen name='QRList' component={QRList} />
+      <Tab.Screen
+        name='ReadQR'
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="qr-code-scanner" size={28} color="black" />
+          )
+        }}
+        component={ReadQR} />
+      <Tab.Screen name='QRList'
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="list" size={28} color="black" />
+          )
+        }}
+        component={QRList} />
     </Tab.Navigator>
   );
 }
