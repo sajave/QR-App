cb4889a2cbc4b7c8dfd47662b592104a5b367214
Santiago <santiagojavierveiga@gmail.com>
2021-09-11 01:09:47 -0300

    feat(client/ReadQR): added barCodeScanner

--- src/Views/ReadQR/ReadQR.js
+++ src/Views/ReadQR/ReadQR.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { View, Text, TouchableOpacity } from 'react-native'
-import { getQRData, putQRData } from '../../../redux/actions';
-import { styles } from '../../../constant/buttonStyle';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { styles } from './ReadQRStyles'
+import { buttonStyles } from '../../../constant/buttonStyle';
 
 export function ReadQR() {
-  const dispatch = useDispatch();
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-  function checkReduxGet() {
-    dispatch(getQRData())
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
   }
-
-  function checkReduxPut() {
-    dispatch(putQRData('example data'))
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
 
   return (
-    <View>
-      <Text>This is the 'ReadQR' view</Text>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={() => checkReduxGet()}>
-          <Text>Get data</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} />
       </View>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={() => checkReduxPut()}>
-          <Text>Put data</Text>
+      {scanned && (
+        <TouchableOpacity style={buttonStyles.button} onPress={() => setScanned(false)}>
+          <Text style={buttonStyles.btnText}>Scan again?</Text>
         </TouchableOpacity>
-      </View>
+      )
+      }
     </View>
   )
 }
