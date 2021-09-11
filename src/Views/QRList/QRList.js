fbbfbb41343c2eeac975b80f1d2cd812d492c729
Santiago <santiagojavierveiga@gmail.com>
2021-09-11 11:39:43 -0300

    feat(client/QRList): added Flatlist

--- src/Views/QRList/QRList.js
+++ src/Views/QRList/QRList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { View, Text, TouchableOpacity, SafeAreaView, FlatList } from 'react-native';
 import { getQRData } from '../../../redux/actions';
-
+import { QRItem } from '../../../components/QRItem';
 import { buttonStyles } from '../../../constant/buttonStyle';
+import { styles } from '../ReadQR/ReadQRStyles';
 
 export function QRList() {
   const dispatch = useDispatch();
+  const scannedQRs = useSelector(state => state.QRdata);
 
   function checkReduxGet() {
     dispatch(getQRData())
@@ -14,12 +16,21 @@ export function QRList() {
 
   return (
     <View style={buttonStyles.container}>
-      <Text>This is the 'QRList' view</Text>
       <View >
         <TouchableOpacity style={buttonStyles.button} onPress={() => checkReduxGet()}>
           <Text style={buttonStyles.btnText}>Get data</Text>
         </TouchableOpacity>
       </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={scannedQRs}
+          renderItem={({ item, index }) => (
+            <QRItem
+              id={index}
+              name={item} />
+          )}
+        />
+      </SafeAreaView>
     </View>
   )
 }
