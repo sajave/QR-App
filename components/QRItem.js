3b6a53f4505921ec419c442452c7a95722495531
Santiago <santiagojavierveiga@gmail.com>
2021-09-11 15:18:37 -0300

    feat(client/QRItem): added id and date scanned to item

--- components/QRItem.js
+++ components/QRItem.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './QRItemStyles';
 
-export function QRItem({ name, id }) {
+export function QRItem({ url, id, date }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.font}>Code: {name}</Text>
+      <Text style={styles.font}>Scanned on {date}</Text>
+      <Text style={styles.font}>ID: {id}</Text>
+      <Text style={styles.font}>Code: {url}</Text>
     </View>
   )
 };
