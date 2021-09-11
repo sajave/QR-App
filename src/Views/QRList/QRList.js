66c265abf52bca8c26aea493555e50fdc48f97cb
Santiago <santiagojavierveiga@gmail.com>
2021-09-11 01:10:29 -0300

    feat(client/QRList): added button to check redux

--- src/Views/QRList/QRList.js
+++ src/Views/QRList/QRList.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { useDispatch } from 'react-redux';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { getQRData } from '../../../redux/actions';
+
+import { buttonStyles } from '../../../constant/buttonStyle';
 
 export function QRList() {
+  const dispatch = useDispatch();
+
+  function checkReduxGet() {
+    dispatch(getQRData())
+  };
+
   return (
-    <View>
+    <View style={buttonStyles.container}>
       <Text>This is the 'QRList' view</Text>
+      <View >
+        <TouchableOpacity style={buttonStyles.button} onPress={() => checkReduxGet()}>
+          <Text style={buttonStyles.btnText}>Get data</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
