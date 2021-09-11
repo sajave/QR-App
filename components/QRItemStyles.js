9d994b5255daecdc45da63b70ad8d74b22c16ab5
Santiago <santiagojavierveiga@gmail.com>
2021-09-11 11:41:09 -0300

    feat(client/QRItem): styles for QRItems

---
+++ components/QRItemStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    padding: 5,
+    backgroundColor: '#ffffff',
+  },
+  font: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  image: {
+    resizeMode: "contain",
+    height: 100,
+    width: 200
+  }
+})
