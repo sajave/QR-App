963800bbed62fed733a6cf9bd4cd5fd62396e6b5
Santiago <santiagojavierveiga@gmail.com>
2021-09-12 20:19:31 -0300

    fix(client/QRItem): modified styles

--- components/QRItemStyles.js
+++ components/QRItemStyles.js
@@ -14,5 +14,32 @@ export const styles = StyleSheet.create({
     resizeMode: "contain",
     height: 100,
     width: 200
-  }
+  },
+  containerURL: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  url: {
+    fontSize: 20,
+    fontWeight: "normal",
+  },
+  item: {
+    backgroundColor: '#86c0f7',
+    borderRadius: 10,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  },
 })
