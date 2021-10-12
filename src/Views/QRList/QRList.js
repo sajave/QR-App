677dadb89b94489be652c5bf2fb0faee89edb28c
Santiago <santiagojavierveiga@gmail.com>
2021-10-12 18:05:46 -0300

    feat(client/QRList): added background

--- src/Views/QRList/QRList.js
+++ src/Views/QRList/QRList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { View, SafeAreaView, FlatList, TextInput, Text } from 'react-native';
+import { View, SafeAreaView, FlatList, TextInput, Text, Image } from 'react-native';
 import { QRItem } from '../../../components/QRItem';
 import { styles } from './QRListStyles';
 
@@ -19,44 +19,50 @@ export function QRList() {
 
   if (scannedQRs.length == 0) {
     return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>You don{"'"}t have scanned QRs</Text>
-        </View>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          source={require('../../../assets/Background/app-background-v001.png')}
+          resizeMode='contain'
+          style={{ width: 500, height: 800 }}
+        />
+        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>You don{"'"}t have scanned QRs</Text>
+      </View>
     )
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <TextInput
-          style={styles.input}
-          onChangeText={(event) => searchChange(event)}
-          placeholder="Search by name..."
-          value={inputSearch}
-        />
-        {searchBar.length > 0 ?
-          (
-            <SafeAreaView style={styles.container}>
-              <FlatList
-                data={searchBar.reverse()}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item, index }) => (
-                  <QRItem
-                    url={item.newData}
-                    date={item.dateScanned}
-                    qrName={item.QRName} />
-                )}
-              />
-            </SafeAreaView>
-          ) : (
-            <View style={styles.container}>
-              <Text>No QRs found</Text>
-            </View>
-          )
-        }
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image
+        source={require('../../../assets/Background/app-background-v001.png')}
+        resizeMode='cover'
+        style={{ with: 500, height: 900 }}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(event) => searchChange(event)}
+        placeholder="Search by name..."
+        value={inputSearch}
+      />
+      {searchBar.length > 0 ?
+        (
+          <SafeAreaView style={styles.container}>
+            <FlatList
+              data={searchBar.reverse()}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item, index }) => (
+                <QRItem
+                  url={item.newData}
+                  date={item.dateScanned}
+                  qrName={item.QRName} />
+              )}
+            />
+          </SafeAreaView>
+        ) : (
+          <View style={styles.container}>
+            <Text>No QRs found</Text>
+          </View>
+        )
+      }
+    </View>
   )
 }
