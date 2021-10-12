cb6740171a23a710e7e35e8510eaaa63bb5710b5
Santiago <santiagojavierveiga@gmail.com>
2021-10-12 20:06:41 -0300

    feat(client/QRList): added ImageBackground

--- src/Views/QRList/QRList.js
+++ src/Views/QRList/QRList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { View, SafeAreaView, FlatList, TextInput, Text, Image } from 'react-native';
+import { View, SafeAreaView, FlatList, TextInput, Text, ImageBackground } from 'react-native';
 import { QRItem } from '../../../components/QRItem';
 import { styles } from './QRListStyles';
 
@@ -20,49 +20,68 @@ export function QRList() {
   if (scannedQRs.length == 0) {
     return (
       <View style={styles.container}>
-        <Image
+        <ImageBackground
           source={require('../../../assets/Background/app-background-v001.png')}
-          resizeMode='contain'
-          style={{ width: 500, height: 800 }}
-        />
-        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>You don{"'"}t have scanned QRs</Text>
-      </View>
+          resizeMode='cover'
+          style={{
+            flex: 1,
+            justifyContent: "center"
+          }}
+        >
+          <View style={styles.container}>
+            <Text style={{ position: 'absolute', fontSize: 20, fontWeight: 'bold', color: 'black' }}>You don{"'"}t have scanned QRs</Text>
+          </View>
+        </ImageBackground>
+      </View >
     )
   };
 
   return (
     <View style={styles.container}>
-      <Image
+      <ImageBackground
         source={require('../../../assets/Background/app-background-v001.png')}
         resizeMode='cover'
-        style={{ with: 500, height: 900 }}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(event) => searchChange(event)}
-        placeholder="Search by name..."
-        value={inputSearch}
-      />
-      {searchBar.length > 0 ?
-        (
-          <SafeAreaView style={styles.container}>
-            <FlatList
-              data={searchBar.reverse()}
-              keyExtractor={(item, index) => index.toString()}
-              renderItem={({ item, index }) => (
-                <QRItem
-                  url={item.newData}
-                  date={item.dateScanned}
-                  qrName={item.QRName} />
-              )}
-            />
-          </SafeAreaView>
-        ) : (
-          <View style={styles.container}>
-            <Text>No QRs found</Text>
-          </View>
-        )
-      }
+        style={{
+          flex: 1,
+          justifyContent: "center"
+        }}
+      >
+        <TextInput
+          style={styles.input}
+          onChangeText={(event) => searchChange(event)}
+          placeholder="Search by name..."
+          value={inputSearch}
+        />
+        {searchBar.length > 0 ?
+          (
+            <View style={styles.container}>
+              <ImageBackground
+                source={require('../../../assets/Background/app-background-v001.png')}
+                resizeMode='cover'
+                style={{
+                  flex: 1,
+                  justifyContent: "center"
+                }}
+              >
+                <FlatList
+                  data={searchBar.reverse()}
+                  keyExtractor={(item, index) => index.toString()}
+                  renderItem={({ item, index }) => (
+                    <QRItem
+                      url={item.newData}
+                      date={item.dateScanned}
+                      qrName={item.QRName} />
+                  )}
+                />
+              </ImageBackground>
+            </View>
+          ) : (
+            <View style={styles.container}>
+              <Text>No QRs found</Text>
+            </View>
+          )
+        }
+      </ImageBackground>
     </View>
   )
 }
